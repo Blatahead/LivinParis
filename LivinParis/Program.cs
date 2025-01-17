4e33fcca52567b6435a342eb095784b35b1542c5
Antonin <81589270+Blatahead@users.noreply.github.com>
2025-01-17 01:49:58 +0100

    premiere co bdd local

--- LivinParis/Program.cs
+++ LivinParis/Program.cs
@@ -1,3 +1,6 @@
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+
 namespace LivinParis
 {
     internal static class Program
@@ -8,10 +11,46 @@ namespace LivinParis
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+            // MySQL en parallèle
+            var databaseTask = Task.Run(() => ConnectToDatabase());
+
             Application.Run(new Form1());
+        }
+
+        static void ConnectToDatabase()
+        {
+            string myConnectionString = "server=localhost;uid=root;pwd=rootantomat;database=world";
+
+            try
+            {
+                using var myConnection = new MySql.Data.MySqlClient.MySqlConnection(myConnectionString);
+                myConnection.Open();
+                MessageBox.Show("Connexion réussie !");
+
+                MySqlCommand myCommand = new MySqlCommand
+                {
+                    Connection = myConnection,
+                    CommandText = @"SELECT * FROM city WHERE ID = @cityId;"
+                };
+                myCommand.Parameters.AddWithValue("@cityId", 1);
+
+                using var myReader = myCommand.ExecuteReader();
+                while (myReader.Read())
+                {
+                    var id = myReader.GetInt32("ID");
+                    var name = myReader.GetString("Name");
+                    var countryCode = myReader.GetString("CountryCode");
+                    var population = myReader.GetInt32("Population");
+
+                    MessageBox.Show($"ID: {id}, Name: {name}, CountryCode: {countryCode}, Population: {population}");
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
