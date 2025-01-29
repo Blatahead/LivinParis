47b99068352720680cbfdf8980742f973e148968
Antonin <81589270+Blatahead@users.noreply.github.com>
2025-01-29 11:25:17 +0100

    class Lien

--- ClassLibraryRendu1/Lien.cs
+++ ClassLibraryRendu1/Lien.cs
@@ -8,5 +8,15 @@ namespace ClassLibraryRendu1
 {
     public class Lien
     {
+        Noeud source;
+        Noeud destination;
+        public Noeud Source { get; set; }
+        public Noeud Destination { get; set; }
+
+        public Lien(Noeud source1, Noeud destination1)
+        {
+            this.source = source1;
+            this.destination = destination1;
+        }
     }
 }
