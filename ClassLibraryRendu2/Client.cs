8be3d0dd242900c4c764ee3087fc0a155c8c4fe1
Mathieu ANTUNES--HOLUÉ <mathieu.antunes--holue@edu.devinci.fr>
2025-03-27 18:02:20 +0100

    méthodes Entreprise terminées

--- ClassLibraryRendu2/Client.cs
+++ ClassLibraryRendu2/Client.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,4 +25,45 @@ namespace ClassLibraryRendu2
         #endregion
 
     }
+    public void CreerClient(Client<T> p1)
+    {
+        ConnexionDB.ConnectToDatabase();
+        string demande = "INSERT INTO Utilisateur (Id_utilisateur, Mdp, Mail_utilisateur) VALUES ("+p1.idUser+","+p1.mdp+","+p1.adresse_mail+")";
+        using (MySqlCommand cmd = new MySqlCommand(demande)) ;
+
+
+    }
+
+    public void ModifierClient(Client<T> p1)
+    {
+
+        ConnexionDB.ConnectToDatabase();
+        string demande = "UPDATE SET Utilisateur Id_Utilisateur="+p1.idUser+", Mdp="+p1.mdp+" WHERE Adresse_mail="+p1.adresse_mail+";";
+        using (MySqlCommand cmd = new MySqlCommand(demande)) ;
+
+    }
+
+    public void DeleteClient(Client<T> p1)
+    {
+        
+        
+            ConnexionDB.ConnectToDatabase();
+            string demande = "DELETE FROM Client_ WHERE ="+p1.idUser+";";
+            using (MySqlCommand cmd = new MySqlCommand(demande)) ;
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- ClassLibraryRendu2/Particulier.cs
+++ ClassLibraryRendu2/Particulier.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -39,5 +40,32 @@ namespace ClassLibraryRendu2
             set { adresseParticulier=value; }
         }
         #endregion
+
+
+        public void CreerParticulier(Particulier<T> p1)
+        {
+            ConnexionDB.ConnectToDatabase();
+            string demande = "INSERT INTO Particulier (Id_particulier, Prenom_particulier, Nom_particulier, Adresse_particulier) VALUES ("+p1.idParticulier+","+p1.prenomParticulier+","+p1.nomParticulier+","+p1.adresseParticulier+")";
+            using (MySqlCommand cmd = new MySqlCommand(demande)) ;
+
+
+        }
+        public void ModifierParticulier(Particulier<T> p1)
+        {
+
+            ConnexionDB.ConnectToDatabase();
+            string demande = "UPDATE SET Particulier Id_particulier="+p1.idParticulier+", Prenom_particulier="+p1.prenomParticulier+", Nom_particulier="+p1.nomParticulier+" WHERE Adresse_particulier="+p1.adresseParticulier+";";
+            using (MySqlCommand cmd = new MySqlCommand(demande)) ;
+
+        }
+
+        public void DeleteParticulier(Particulier<T> p1)
+        {
+
+            ConnexionDB.ConnectToDatabase();
+            string demande = "DELETE FROM Particulier WHERE id_particulier="+p1.idParticulier+";";
+            using (MySqlCommand cmd = new MySqlCommand(demande)) ;
+
+        }
     }
 }
