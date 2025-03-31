bc1dbbd63e1a47bd119bef2e459287966d9cbd79
Antonin <81589270+Blatahead@users.noreply.github.com>
2025-03-31 16:08:44 +0200

    nav bar layout

---
+++ LivinParisWebApp/Pages/CuisinierPanel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LivinParisWebApp.Pages
+{
+    public class CuisinierPanelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
