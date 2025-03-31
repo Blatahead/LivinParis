dbb3ec522b310afe33d59994eae2f1e0fd2e7496
Antonin <81589270+Blatahead@users.noreply.github.com>
2025-03-31 12:11:49 +0200

    Ajout page popup any account

--- LivinParisWebApp/Pages/AnyAccount.cshtml.cs
+++ LivinParisWebApp/Pages/AnyAccount.cshtml.cs
@@ -8,5 +8,17 @@ namespace LivinParisWebApp.Pages
         public void OnGet()
         {
         }
+
+        //penser à rediriger vers le panel (login pour test)
+        public IActionResult OnPostLogin()
+        {
+            return RedirectToPage("/Login");
+        }
+
+        // pas besoin de récup les valeurs mises dans login : on redirige vers register directement
+        public IActionResult OnPostRegister()
+        {
+            return RedirectToPage("/Register");
+        }
     }
 }
