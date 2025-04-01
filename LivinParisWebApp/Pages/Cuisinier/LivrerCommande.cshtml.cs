2238065efc4f2364477301d70e7186348ef57629
Antonin <81589270+Blatahead@users.noreply.github.com>
2025-04-01 15:09:09 +0200

    btn see commands en cours + page commandes a livrer

---
+++ LivinParisWebApp/Pages/Cuisinier/LivrerCommande.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LivinParisWebApp.Pages.Cuisinier
+{
+    public class LivrerCommandeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ LivinParisWebApp/Pages/Cuisinier/RefuseCommande.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LivinParisWebApp.Pages.Cuisinier
+{
+    public class RefuseCommandeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
