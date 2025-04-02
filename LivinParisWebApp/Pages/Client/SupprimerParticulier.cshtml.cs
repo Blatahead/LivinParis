fff14ca9771a1972e145fbec0041866fe9a881f1
Antonin <81589270+Blatahead@users.noreply.github.com>
2025-04-02 03:27:03 +0200

    pages suppression cuisinier, entreprise, particluer + init settings client

---
+++ LivinParisWebApp/Pages/Client/SupprimerParticulier.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LivinParisWebApp.Pages.Client
+{
+    public class SupprimerParticulierModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPostNo()
+        {
+            return RedirectToPage("/Client/SettingsClient");
+        }
+
+        public IActionResult OnPostYes()
+        {
+            return RedirectToPage("/Login");
+        }
+    }
+}
