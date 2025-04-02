4c3fd658adbb6aa8b297c30f78a3557e7019073a
Antonin <81589270+Blatahead@users.noreply.github.com>
2025-04-02 03:15:56 +0200

    pages details commande client et cuisinier update

---
+++ LivinParisWebApp/Pages/Client/LivrerCommande.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LivinParisWebApp.Pages.Client
+{
+    public class LivrerCommandeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
