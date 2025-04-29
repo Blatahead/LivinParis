fc585d50b19ecabefaf2341998de031b37abd17f
Mathieu ANTUNES--HOLUÉ <mathieu.antunes--holue@edu.devinci.fr>
2025-04-29 18:18:10 +0200

    Ajout des 10 classes DTO pour les requêtes SQL complexes

---
+++ ClassLibrary/CuisinierSansCommandeDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class CuisinierSansCommandeDTO
+    {
+        public int ID_cuisinier { get; set; }
+        public string Nom { get; set; }
+    }
+
+}
